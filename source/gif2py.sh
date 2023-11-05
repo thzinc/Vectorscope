1f6fec8bd0eded7b7619c7e14b1579db5fc866db
Daniel James <daniel@thzinc.com>
2023-11-05 09:57:21 -0800

    Compress the images more for faster rendering/playback
    - Quality doesn't matter much at the distances one might be looking at the badge

--- source/gif2py.sh
+++ source/gif2py.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 GIF_PATH="$1"
 ANIMATION_NAME="$2"
 
-convert "$GIF_PATH" -coalesce -resize 1000x240 -gravity Center -crop "240x240+0+0" -type TrueColor "${ANIMATION_NAME}%02d.jpg"
+convert "$GIF_PATH" -coalesce -resize 1000x240 -gravity Center -crop "240x240+0+0" -quality 10 -type TrueColor "${ANIMATION_NAME}%02d.jpg"
